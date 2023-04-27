0d11907666abd192645656a59ed4e4b50ed4ddc3
Yevhen <pelikh123.evg@gmail.com>
2023-04-27 18:34:17 +0200

    prints change

--- modo/modo_1.py
+++ modo/modo_1.py
@@ -16,31 +16,35 @@ if f1 <= (round(f2 + f3, -3)):
 else:
     print("Функція не опукла")
 
-mx = 1
 x1 = 0.618 * a + 0.382 * b
 x2 = 0.382 * a + 0.618
 
 fx1 = m * (pow(x1, 2)) + n * x1 + c
 fx2 = m * (pow(x2, 2)) + n * x2 + c
 
-while b-a > epsilon:
+count_i = 0
+while b - a >= epsilon:
+    count_i += 1
+
     if fx1 <= fx2:
         b = x2
         x2 = x1
         x1 = 0.618 * a + 0.382 * b
         fx2 = fx1
         fx1 = m * (pow(x1, 2)) + n * x1 + c
-        mx = mx + 1
-        print(a, b, a-b, x1, x2, fx1, fx2)
+        print(f"K: {count_i}), a: {a},  b: {b}, a-b: {a - b}, x1: {x1}, x2: {x2}, fx1: {fx1}, fx2: {fx2}")
     else:
         a = x1
         x1 = x2
         x2 = 0.382 * a + 0.618 * b
         fx1 = fx2
         fx2 = m * (pow(x2, 2)) + n * x2 + c
-        mx = mx + 1
-        print(a, b, a - b, x1, x2, fx1, fx2)
+        print(f"K: {count_i}), a: {a},  b: {b}, a-b: {a - b}, x1: {x1}, x2: {x2}, fx1: {fx1}, fx2: {fx2}")
+
+        last_value_a = a
+        last_value_b = b
 
 x = (a + b) / 2
 fx = m * (pow(x, 2)) + n * x + c
-print(fx, x)
+print(f"fx= {fx}, x= {x}")
+print("l=", last_value_b - last_value_a, "=", pow(0.618, count_i) * (b - a))
