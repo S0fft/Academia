6e85feb049b9482e2dcf1997a0b38ca4b5f3687e
Yevhen <pelikh123.evg@gmail.com>
2023-05-08 19:04:29 +0200

    modo_3-lab

---
+++ modo/modo_3.py
@@ -0,0 +1,33 @@
+m = 7
+n = 39
+X0 = 49
+tol = 0.004
+
+
+def f(x, m, n):
+    return n * x ** 2 + m / x
+
+
+def f_deriv(x, m, n):
+    return 2 * n * x - m / x ** 2
+
+
+def f_second_deriv(x, m, n):
+    return 2 * n + 2 * m / x ** 3
+
+
+def newton_raphson(x0, m, n, tol):
+    x = x0
+    while True:
+        fx = f(x, m, n)
+        fprime_x = f_deriv(x, m, n)
+        fprimeprime_x = f_second_deriv(x, m, n)
+        delta_x = -fprime_x / fprimeprime_x
+        x += delta_x
+        if abs(delta_x) < tol:
+            break
+    return x
+
+
+x_min = newton_raphson(X0, m, n, tol)
+print("Минимум функции:", x_min)
