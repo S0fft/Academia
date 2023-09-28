7a50bdfab8050d589c56098efd43e7ad9424a976
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2023-09-28 10:49:17 +0200

    chm-2 - added omega

--- chm/chm_2.py
+++ chm/chm_2.py
@@ -1,11 +1,11 @@
-def simple_iteration_solver(B, c, accuracy=0.001, max_iterations=500):
+def simple_iteration_solver(B, c, omega=1.0, accuracy=0.001, max_iterations=500):
     n = len(c)
     x = [0.0] * n
     for i in range(max_iterations):
         x_new = [0.0] * n
         for j in range(n):
             sum_ = sum(B[j][k] * x[k] for k in range(n) if k != j)
-            x_new[j] = (c[j] - sum_) / B[j][j]
+            x_new[j] = omega * ((c[j] - sum_) / B[j][j]) + (1 - omega) * x[j]
         if all(abs(x_new[j] - x[j]) < accuracy for j in range(n)):
             return x_new
         x = x_new
@@ -19,5 +19,6 @@ B = [
 ]
 c = [4.5, -3.2, 5.6]
 
-solution = simple_iteration_solver(B, c, accuracy=0.001)
-print("Рішення:", solution)
+w = 0.1
+solution = simple_iteration_solver(B, c, omega=w, accuracy=0.001)
+print("Решение:", solution)
