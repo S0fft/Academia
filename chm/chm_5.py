21640b379c85f01a731f9b51d3f2845f15eced23
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2023-10-25 16:24:03 +0200

    changed chm5 - added double method

--- chm/chm_5.py
+++ chm/chm_5.py
@@ -32,22 +32,22 @@ def simpson_method(a, b, n):
 desired_order = 0.01
 desired_error_estimate = 0.0001
 
-
 n = 1
 
 while True:
     n *= 2
     integral_right_triangle = right_triangle_method(0.6, 1.5, n)
+
     integral_simpson = simpson_method(0.6, 1.5, n)
+    integral_simpson_double_count = simpson_method(0.6, 1.5, 2 * n)
 
     error_right_triangle = abs(integral_right_triangle - integral_simpson) / 15
-    error_simpson = abs(integral_simpson - integral_right_triangle) / 15
+    error_simpson = abs(integral_simpson - integral_simpson_double_count) / 15
 
     if error_right_triangle < desired_order and error_simpson < desired_order:
         break
 
 print("Інтеграл методом правих прямих трикутників:", integral_right_triangle)
-print("Інтегральний метод парабол (формула Сімпсона):", integral_simpson)
+print("Похибка методу Сімпсона з подвійним перерахунком:", error_simpson)
 print("Кількість розбиття:", n)
 print("Похибка методу правих прямих трикутників:", error_right_triangle)
-print("Похибка методу параболу (формула Сімпсона):", error_simpson)
