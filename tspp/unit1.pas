f1408ed31e6530da6bd8d306d4988a07bb26aa18
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2024-02-15 19:02:44 +0100

    added tspp

---
+++ tspp/unit1.pas
@@ -0,0 +1,44 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+  Form2: TForm2;
+
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Form2 := TForm2.Create(Self);
+  Form2.Show;
+end;
+
+end.
+
