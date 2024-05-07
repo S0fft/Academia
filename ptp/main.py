b3e791ea813bfc872b58c5e5ef248dca5c254640
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2024-05-07 09:23:38 +0200

    remove comments

--- ptp/main.py
+++ ptp/main.py
@@ -19,22 +19,18 @@ class ScheduleSystem:
 
 
 if __name__ == "__main__":
-    # Создаем объект системы составления расписания
     schedule_system = ScheduleSystem()
 
-    # Добавляем курсы и их предварительные условия
     schedule_system.add_course("Аналіз даних та знань")
     schedule_system.add_course("WEB-технології та WEB-дізайн-2", prerequisites=["Аналіз даних та знань"])
-    schedule_system.add_course("Теорія інформації	", prerequisites=["Аналіз даних та знань"])
-    schedule_system.add_course("Іноземна мова", prerequisites=["Теорія інформації	", "Аналіз даних та знань"])
+    schedule_system.add_course("Теорія інформації", prerequisites=["Аналіз даних та знань"])
+    schedule_system.add_course("Іноземна мова", prerequisites=["Теорія інформації", "Аналіз даних та знань"])
     schedule_system.add_course("Основи охорони праці", prerequisites=["Тайм-менеджмент", "Теорія інформації"])
 
-    # Генерируем расписание занятий
     schedule = schedule_system.generate_schedule()
     print("Расписание занятий:")
     for index, course in enumerate(schedule, start=1):
         print(f"{index}. {course}")
 
-    # Визуализация графа предварительных условий
     nx.draw(schedule_system.schedule, with_labels=True, font_weight='bold')
     plt.show()
